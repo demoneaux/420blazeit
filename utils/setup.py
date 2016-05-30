0a117e6e04ea94c620bf13a31c9fbe8e3d397c64
Benjamin Tan <demoneaux@gmail.com>
2016-05-30 20:34:50 +0800

    Enable remote API.

--- utils/setup.py
+++ utils/setup.py
@@ -3,14 +3,15 @@ from app.models import Blazer
 genders = ['M', 'F']
 sizes = ['XS', 'S', 'M', 'L', 'XL']
 
-for (_, gender) in enumerate(genders):
-    for (_, size) in enumerate(sizes):
-        for i in range(10):
-            serial_number = gender + str(i) + size
-            blazer = Blazer(
-                serial_number=serial_number,
-                size=size,
-                gender=gender,
-                booked=False
-            )
-            blazer.put()
+def init:
+    for (_, gender) in enumerate(genders):
+        for (_, size) in enumerate(sizes):
+            for i in range(10):
+                serial_number = gender + str(i) + size
+                blazer = Blazer(
+                    serial_number=serial_number,
+                    size=size,
+                    gender=gender,
+                    booked=False
+                )
+                blazer.put()
