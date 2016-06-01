0e50fcfaf4dd4e6eae9f533dc47aae7456ab1184
Benjamin Tan <demoneaux@gmail.com>
2016-06-01 18:48:11 +0800

    Remove code for populating database.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,8 @@
 # __init__.py
 
-from . import home, listings, blazer, populate, oauth
+from . import home, listings, blazer, oauth
 
 home = home.app
 listings = listings.app
 blazer = blazer.app
-populate = populate.app
 oauth = oauth.app
